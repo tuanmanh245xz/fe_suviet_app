926b1e63d751b27251aefe073e1b53b5bb0ea14e
tuanmanh245xz <tuanmanh435@gmail.com>
2024-09-25 22:14:40 +0700

    add react-native-icons-vector

--- src/screens/auth/login/LoginScreen.tsx
+++ src/screens/auth/login/LoginScreen.tsx
@@ -1,12 +1,20 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, ImageBackground, TextInput } from 'react-native';
 import React from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Reponse } from '../../../constants/Reponse';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-      <Button title="Login" onPress={async () => await AsyncStorage.setItem('asseccToken','')}/>
+    <View style={{  alignItems:'center'}}>
+     <View style={{alignItems:'center',marginTop:Reponse.sizes.height * 0.1}}>
+     <Text style={{ fontSize:20}}>Sign In</Text>
+     <ImageBackground source={require('../../../assets/image/Page-1.png')} style={{width:Reponse.sizes.width * 0.7, height:Reponse.sizes.height * 0.4}} resizeMode="contain"/>
+     </View>
+     <View>
+        <TextInput placeholder="Email"/>
+     </View>
+      {/* <Button title="Login" onPress={async () => await AsyncStorage.setItem('asseccToken','')}/> */}
+
     </View>
   );
 };
